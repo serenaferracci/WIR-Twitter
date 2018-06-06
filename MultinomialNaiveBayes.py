a65c14ad619ba86f1c1ba2dded5f3408f478b6ca
serenaferracci <serenaferracci@github.com>
2018-06-06 17:19:24 +0200

    start learning

--- MultinomialNaiveBayes.py
+++ MultinomialNaiveBayes.py
@@ -3,7 +3,9 @@ import string
 from sklearn.datasets import load_files
 from sklearn.model_selection import train_test_split
 
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 
 from nltk.corpus import stopwords
 from nltk.stem.snowball import EnglishStemmer
@@ -31,8 +33,8 @@ def stemming_tokenizer(text):
 
 
 ## Dataset containing Positive and neative sentences on Amazon products
-data_folder_training_set = "./Amazon/Training"
-data_folder_test_set     = "./Amazon/Test"
+data_folder_training_set = "./Tweets_train"
+data_folder_test_set     = "./Tweets_test"
 
 training_dataset = load_files(data_folder_training_set)
 test_dataset = load_files(data_folder_test_set)
@@ -70,99 +72,28 @@ print(target_names)
 print("----------------------")
 
 
+# Conversion of the Training Set
+count_vect = CountVectorizer("")
+X_train_counts = count_vect.fit_transform(X_train)
+print("")
+print("----------------------")
+print("Training Set conversion:")
+print("")
+print("NumOccurrences Documents X Terms Matrix Representation of Training Set")
+print(X_train_counts.shape)
+print("")
+tfidf_transformer = TfidfTransformer("")
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+print("")
+print("Tf-idf Documents X Terms Matrix Representation of Training Set")
+print(X_train_tfidf.shape)
+print("----------------------")
+print("")
 
-## Vectorization object
-vectorizer = TfidfVectorizer(strip_accents= None,
-							preprocessor = None,
-							)
 
 ## classifier
-nbc = MultinomialNB()
-classifier = KNeighborsClassifier()
-
-
-
-## With a Pipeline object we can assemble several steps
-## that can be cross-validated together while setting different parameters.
-
-pipeline = Pipeline([
-	('vect', vectorizer),
-	#('nbc', nbc),
-	('classifier', classifier),
-	])
-
-
-## Setting parameters.
-## Dictionary in which:
-##  Keys are parameters of objects in the pipeline.
-##  Values are set of values to try for a particular parameter.
-parameters = {
-	'vect__tokenizer': [None, stemming_tokenizer],
-	'vect__ngram_range': [(1, 1), (1, 2),],
-	#'nbc__alpha': [.001,.01, 1.0, 10.],
-	'classifier__n_neighbors': [3,6,9],
-	}
-
-
-## Create a Grid-Search-Cross-Validation object
-## to find in an automated fashion the best combination of parameters.
-grid_search = GridSearchCV(pipeline,
-						   parameters,
-						   scoring=metrics.make_scorer(metrics.average_precision_score, average='weighted'),
-						   cv=3,
-						   n_jobs=2,
-						   verbose=10)
-
-## Start an exhaustive search to find the best combination of parameters
-## according to the selected scoring-function.
 print("")
-grid_search.fit(X_train, Y_train)
-print("")
-
-## Print results for each combination of parameters.
-number_of_candidates = len(grid_search.cv_results_['params'])
-print("Results:")
-for i in range(number_of_candidates):
-	print(i, 'params - %s; mean - %0.3f; std - %0.3f' %
-			(grid_search.cv_results_['params'][i],
-			grid_search.cv_results_['mean_test_score'][i],
-			grid_search.cv_results_['std_test_score'][i]))
-
-print("")
-print("Best Estimator:")
-pp.pprint(grid_search.best_estimator_)
-print("")
-print("Best Parameters:")
-pp.pprint(grid_search.best_params_)
-print("")
-print("Used Scorer Function:")
-pp.pprint(grid_search.scorer_)
-print("")
-print("Number of Folds:")
-pp.pprint(grid_search.n_splits_)
-print("")
-
-
-
-#Let's train the classifier that achieved the best performance,
-# considering the select scoring-function,
-# on the entire original TRAINING-Set
-Y_predicted = grid_search.predict(X_test)
-
-# Evaluate the performance of the classifier on the original Test-Set
-output_classification_report = metrics.classification_report(
-									Y_test,
-									Y_predicted,
-									target_names=target_names)
-print("")
-print("----------------------------------------------------")
-print(output_classification_report)
-print("----------------------------------------------------")
-print("")
-
-# Compute the confusion matrix
-confusion_matrix = metrics.confusion_matrix(Y_test, Y_predicted)
-print("")
-print("Confusion Matrix: True-Classes X Predicted-Classes")
-print(confusion_matrix)
+print(" Start Learning...")
+classifier = MultinomialNB().fit(X_train_tfidf, Y_train)
+print(" End Learning.")
 print("")
