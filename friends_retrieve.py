34063c8a8fbcfb76bc7fcf9c13cca6d953ead2f3
serenaferracci <serenaferracci@github.com>
2018-06-07 15:06:32 +0200

    modified friends_retrieve code

--- friends_retrieve.py
+++ friends_retrieve.py
@@ -4,19 +4,60 @@ import os
 import sys
 import time
 
+
+if (len(sys.argv)!=2):
+    print("You have to specify an initial seed!")
+    exit(1)
+
+#seed value indicates which folders it selects
+seed=int(sys.argv[1]) #please write an integer as parameter
+
+#seed=0 Emanuele
+#seed=1 Serena
+#seed=2 Ludovica
+
 usa_location = 23424977
 
+consumer_key=""
+consumer_secret=""
+access_token=""
+access_token_secret=""
 
-# Emanuele's keys
-consumer_key = 	"tVyk6jPU4UPBnaGdHMCGvAY48"
-consumer_secret = "QAEViQSW6xE5wRVHwJQP8asDnwXgCmWoN06WE6ep1hgKgIPciQ"
+if seed==0:
+    # Emanuele's keys
+    consumer_key = 	"tVyk6jPU4UPBnaGdHMCGvAY48"
+    consumer_secret = "QAEViQSW6xE5wRVHwJQP8asDnwXgCmWoN06WE6ep1hgKgIPciQ"
 
-access_token = "841341908957954051-PSaSB6RVihZgvCz0RIlYHr3T4ZGg21E"
-access_token_secret = "VMIhfKGvVM9FRMrdq7L1Sy5xWHXid7NF7g7Mc1ZO8JxZi"
+    access_token = "841341908957954051-PSaSB6RVihZgvCz0RIlYHr3T4ZGg21E"
+    access_token_secret = "VMIhfKGvVM9FRMrdq7L1Sy5xWHXid7NF7g7Mc1ZO8JxZi"
+elif seed==1:
+    #Serena's keys
+    consumer_key = 	"MNIY1db3L8UIWbWwcIufdbLsE"
+    consumer_secret = "EI9DxGIM7BoJOzNHUPjTn3kIPwK8OjNTWgLeWlb46wMg5gvFmE"
 
+    access_token = "455678422-G3uLg4O7ulLaZbyVhaSa2zPZamyHjC6eUClw0hUC"
+    access_token_secret = "gGTh7L7O5P7EhsKiqcESXqA9RXjiafNZOrc5MVXBEap4i"
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+elif seed==2:
+    #Ludo's keys
+    consumer_key="dDEH0jtYRRvXPsx55LJqF86kq"
+    consumer_secret="Bt5UWBvE3gMZQkDzdCU5Iuk0gfooGxVR9dYai76WEzxhLKSnaM"
+
+    access_token="315710007-dTyuxZoGhBGFCMrarAqt27WaYuKNSBW2P5IZKtLA"
+    access_token_secret="xUqFFO3LFroFjgO5XVAW1MYkYQhHoxmyLzMAwtHFh8Do1"
+
+else:
+    #Riccardo's key
+    consumer_key = 'wgyF8eM0142tmip6dwNFNR7gl'
+    consumer_secret = 'rGPAlNBF5nrSE6fofq23Bt5BwlBNpd1kLhjUofhEw2dMmoTv7V'
+
+    access_token="1000293863163129856-GoWCSNnKiOm03b4KgrSOdkgeUcJZ99"
+    access_token_secret="2QURVPydYfJfiF8u36ZS7NqfewT1QfgTfVZSSjWnQGH50"
+
+#auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+#auth.set_access_token(access_token, access_token_secret)
+
+auth=tweepy.AppAuthHandler(consumer_key, consumer_secret)
 
 # Creation of the actual interface, using authentication
 api = tweepy.API(auth)
@@ -38,25 +79,46 @@ users = []
         print 'Getting page {} for friends'.format(page_count)
         users.extend(user)
 '''
-for dir in os.listdir("Tweets"):
-    for file in os.listdir("Tweets/" + dir):
+
+directory=os.listdir("Tweets")
+i=0
+for i in range(0, len(directory)):
+    if i%4!=seed:   #this is not your folder, please continue
+        continue
+    dir=directory[i]
+    listdir=os.listdir("Tweets/"+dir)
+    for file in listdir:
+        if (".txt" in file):
+            continue
+        if ((""+file + " - followers.txt") in listdir):
+            print("["+time.ctime()+"] Friends for user "+ file+" yet retrieved")
+            continue
         while True:
             try:
-                ids_fr = []
+                print("["+time.ctime()+"] Retrieving friends for user "+file)
+                ids_fl = []
                 page_count = 0
                 for page in tweepy.Cursor(api.friends_ids, id=file, count=5000).pages():
                     page_count += 1
-                    ids_fr.extend(page)
-                out_fr = open("Tweets/" + dir + "/" + file + " - friends.txt", "w")
-                for id in ids_fr:
-                    out_fr.write(str(id) + "\n")
-                out_fr.close()
+                    ids_fl.extend(page)
+                out_fl = open("Tweets/" + dir + "/" + file + " - followers.txt", "w")
+                for id in ids_fl:
+                    out_fl.write(str(id) + "\n")
+                out_fl.close()
 
-                print("~~~~~~~\n")
+                print("["+time.ctime()+"] Retrieved friends for user "+file)
                 break
-            except tweepy.RateLimitError:
-                print("["+time.ctime()+"] Tweets limit reached, retry in 15 minutes...")
-                time.sleep(910)
-                print("["+time.ctime()+"] Retry now...")
-                continue;
+            except tweepy.TweepError as e:
+                print("["+time.ctime()+"] "+e.reason);
+                if("88" in e.reason):
+                    status=api.rate_limit_status()
+                    for elem in status["resources"]:
+                        #if int(status["resources"][elem]["remaining"])==0:
+                        for elem2 in status["resources"][elem]:
+                            if status["resources"][elem][elem2]["remaining"]==0:
+                                print(status["resources"][elem])
+                    time.sleep(300)
+                    print("["+time.ctime()+"] Retry now...")
+                    continue;
+                break
     print("["+time.ctime()+"] End of the process, collected friends and followers!")
